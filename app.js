f31b99eacc59aef0ac6b10b0c18429a08aeb7ee5
eray <akyildiz379@gmail.com>
2017-06-21 02:09:19 +0300

    Update.

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 #!/usr/bin/env node
 
 // Library
+const prompt = require('prompt');
 const fs = require('fs');
+const fse = require('fs-extra');
 
 // 
 // Start Console App 
 // 
-fs.readdir("AddSortedFiles", (ex, files) => {
- 
-    if ( !ex )
-	{
-		for( var i = 1; i < files.length; i++ )
-		{
-			console.log("File => " + files[i] + " New Name => " + i );
-			
-			fs.rename(`AddSortedFiles/${files[i]}`, `AddSortedFiles/${i}`, (err) => {
-				if ( err )
-					console.log('ERROR: ' + err);
-			});
-		}
-    }
-	else
-		console.log(ex);
+prompt.start();
+
+console.log("What number do you start with ?");
+
+prompt.get(['number'], (err, result) => {
+
+	fse.remove('output',()=>{
+
+		fs.mkdir('output',(e)=>{
+
+			fse.copy('AddSortedFiles', 'output')
+				
+				.then(() => {
+
+					fs.readdir("output", (ex, files) => {
+		
+						if ( !ex )
+						{
+							
+							for( var i = 1; i < files.length; i++ )
+							{
+
+								fs.rename(`output/${files[i]}`, `output/${result.number++}`, (err) => {
+									if ( err )
+										console.log('ERROR: ' + err);
+								});
+
+							}
+
+							console.log(" SUCCESSFUL => In the outputs file ");
+
+						}
+						else
+							console.log(ex);
+					
+					});
+
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		});
+	});
 
 });
