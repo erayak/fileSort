39c216ae37d8abcc0a6847d4b7701c79be794a4f
eray <akyildiz379@gmail.com>
2017-06-20 16:28:43 +0300

    Added Files.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# fileSort
+# Node js File Sort 
+
+***
+
+### Installation
+
+```sh
+$ npm install
+$ node app.js
+```
+
+### USE
+1) Put the files you want to sort into the `AddSortedFiles` folder.
+2) Follow installation steps.
---
+++ app.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+// Library
+const fs = require('fs');
+
+// 
+// Start Console App 
+// 
+fs.readdir("AddSortedFiles", (ex, files) => {
+ 
+    if ( !ex ) 
+	{
+		for( var i in files )
+		{
+			console.log("File => " + files[i] + " New Name => " + i );
+			
+			fs.rename(`AddSortedFiles/${files[i]}`, `AddSortedFiles/${i}`, (err) => {
+				if ( err )
+					console.log('ERROR: ' + err);
+			});
+		}
+    } 
+	else
+		console.log(ex);
+
+});
